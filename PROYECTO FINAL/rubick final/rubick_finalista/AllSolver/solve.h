bbeac719af9f1cc61e0faf1e1cacf56cf3788f0e
Mauricio Carazas Segovia <mauriciocarazas9812@gmail.com>
2024-07-01 12:20:42 -0500

    Add files via upload

---
+++ PROYECTO FINAL/rubick final/rubick_finalista/AllSolver/solve.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include<vector>
+std::string solver(char* cube);
+std::vector<std::string> get_solution(std::string Cube);
